14b7efd777d2221642678e094298c4c7256f8155
ganesanbbc <ganesan.ns@cognizant.com>
2016-11-23 07:25:18 +0530

    first commit

---
+++ LayoutDemo/LayoutDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LayoutDemo
+//
+//  Created by Varshika on 23/11/16.
+//  Copyright © 2016 SwiftPlus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
