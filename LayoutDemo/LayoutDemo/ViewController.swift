7be1c30cc7c5219deba73e333ae6b6bc07d309d0
ganesanbbc <ganesan.ns@cognizant.com>
2016-11-26 12:56:04 +0530

    Label change based on user click

--- LayoutDemo/LayoutDemo/ViewController.swift
+++ LayoutDemo/LayoutDemo/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.label.text = String(counter)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func preButtonClick(_ sender: Any) {
+        self.counter = self.counter-1;
+        self.label.text = String(self.counter)
+    }
+    
 
+    @IBAction func nextBtnClick(_ sender: Any) {
+        self.counter = self.counter+1;
+        self.label.text = String(self.counter)
+
+    }
 
 }
 
